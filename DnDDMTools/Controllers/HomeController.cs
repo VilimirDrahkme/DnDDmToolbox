0236673036e4a74103eccea45596ce1db41c6843
Spencer <spncrdavis47@gmail.com>
2016-11-19 00:54:44 -0600

    started adding AJAX to the homecontroller.

--- DnDDMTools/Controllers/HomeController.cs
+++ DnDDMTools/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DnDDMTools.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,21 +11,68 @@ namespace DnDDMTools.Controllers
     {
         public ActionResult Index()
         {
+
+
+
+
+
+
+
             return View();
         }
-
-        public ActionResult About()
+        public JsonResult PopulateSubraces(int id)
         {
-            ViewBag.Message = "Your application description page.";
+            RaceModel[] subraces = null;
+            switch (id)
+            {
+                case 1:
+                    //user picked Dwarf
+                    RaceModel HillDwarf = new RaceModel(0, 0, 2, 0, 1, 0, 4, 3, "25 feet or 5 5x5 squares", 12, 12);
+                    RaceModel MountainDwarf = new RaceModel(2, 0, 2, 0, 0, 0, 4, 2, "25 feet or 5 5x5 squares", 12, 12);
+                    subraces = new RaceModel[2] { HillDwarf, MountainDwarf };
+                    break;
+                case 2:
+                    //user picked elf
+                    RaceModel HighElf = new RaceModel(0, 2, 0, 1, 0, 0, 6, 3, "30 feet or 6 5x5 squares", 12, 12);
+                    RaceModel WoodElf = new RaceModel(0, 2, 0, 0, 1, 0, 6, 3, "35 feet or 7 5x5 squares", 12, 12);
+                    RaceModel Drow = new RaceModel(0, 2, 0, 0, 0, 1, 6, 3, "30 feet or 6 5x5 squares", 12, 12);
+                    subraces = new RaceModel[3] { HighElf, WoodElf, Drow };
+                    break;
+                case 3:
+                    //user picked halfling
+                    RaceModel Lightfoot = new RaceModel(0, 2, 0, 0, 0, 1, 3, 1, "25 feet or 5 5x5 squares", 6, 6);
+                    RaceModel Stout = new RaceModel(0, 2, 1, 0, 0, 0, 1, 3, "25 feet or 5 5x5 squares", 6, 6);
+                    subraces = new RaceModel[2] { Lightfoot, Stout };
+                    break;
+                case 4:
+                    //Human :P boring right?
+                    RaceModel Human = new RaceModel(1, 1, 1, 1, 1, 1, 6, 4, "30 feet or 6 5x5 squares", 6, 6);
+                    RaceModel HumanVARIANT = new RaceModel(0, 0, 0, 0, 0, 0, 6, 4, "30 feet or 6 5x5 squares", 6, 6);
+                    //come back for the variant for noting that >user< picks his stat allocations as well as one feat and one skill
+                    subraces = new RaceModel[2] { Human, HumanVARIANT };
+                    break;
+                case 5:
+                    //Dragonborn - fucking a dude. yes.
+                    //unfortunately, no alts here :(
+                    break;
 
-            return View();
-        }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+                default:
+                    break;
+            }
 
-            return View();
+
+
+
+
+            if (subraces != null)
+            {
+                return Json(subraces, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json("sorry....no available subraces at this time", JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
---
+++ DnDDMTools/Models/PlayerModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DnDDMTools.Models
+{
+    public class PlayerModel
+    {
+        public RaceModel Race { get; set; }
+    }
+
+    public class RaceModel
+    {
+        private int _minFoot;
+        private int _minInch;
+        private int _maxFoot;
+        private int _maxInch;
+
+
+        public int STRMod { get; set; }
+        public int DEXMod { get; set; }
+        public int CONMod { get; set; }
+        public int INTMod { get; set; }
+        public int WISMod { get; set; }
+        public int CHAMod { get; set; }
+
+        public string[] FemaleNames { get; set; }
+        public string[] MaleNames { get; set; }
+        public int Age { get; set; }
+        public string Alignment { get; set; }
+        public string Size { get; set; }
+        public int MaxFoot { get; set; }
+        public int MinFoot { get; set; }
+        public int MaxInch { get; set; }
+        public int MinInch { get; set; }
+        public int Weight { get; set; }
+        public string Speed { get; set; }
+
+
+
+
+        public RaceModel(int strmod, int dexmod, int conmod, int intmod, int wismod, int chamod, string[] femnames,string[]malenames,
+                        int age, string alignment, string size, int maxfoot, int minfoot, int weight, string speed, int mininch, int maxinch)
+        {
+            STRMod = strmod;
+            DEXMod = dexmod;
+            CONMod = conmod;
+            INTMod = intmod;
+            WISMod = wismod;
+            CHAMod = chamod;
+            FemaleNames = femnames;
+            MaleNames = malenames;
+            Age = age;
+            Alignment = alignment;
+            Size = size;
+            MaxFoot = maxfoot;
+            MinFoot = minfoot;
+            Weight = weight;
+            Speed = speed;
+            MaxInch = maxinch;
+            MinInch = mininch;
+        }
+        /// <summary>
+        /// SubRaceModel
+        /// </summary>
+        /// <param name="strmod"></param>
+        /// <param name="dexmod"></param>
+        /// <param name="conmod"></param>
+        /// <param name="intmod"></param>
+        /// <param name="wismod"></param>
+        /// <param name="chamod"></param>
+        /// <param name="maxfoot"></param>
+        /// <param name="minfoot"></param>
+        /// <param name="speed"></param>
+        /// <param name="mininch"></param>
+        /// <param name="maxinch"></param>
+        public RaceModel(int strmod, int dexmod, int conmod, int intmod, int wismod, int chamod,
+                       int maxfoot, int minfoot, string speed, int mininch, int maxinch)
+        {
+            STRMod = strmod;
+            DEXMod = dexmod;
+            CONMod = conmod;
+            INTMod = intmod;
+            WISMod = wismod;
+            CHAMod = chamod;
+            MaxFoot = maxfoot;
+            MinFoot = minfoot;
+            Speed = speed;
+            MaxInch = maxinch;
+            MinInch = mininch;
+        }
+    }
+}
