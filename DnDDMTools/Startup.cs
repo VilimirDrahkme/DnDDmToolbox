15964759e038b5fadee994f21cbd41f7c0ff2348
spncrdavis47 <spncrdavis47@gmail.com>
2016-11-18 22:55:02 -0600

    Initial Commit

---
+++ DnDDMTools/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DnDDMTools.Startup))]
+namespace DnDDMTools
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
