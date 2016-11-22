dbb0fd02cfa4226b3420042f9552a94106bef5a8
Spencer <spncrdavis47@gmail.com>
2016-11-21 21:34:22 -0600

    Added DB Functionality. not all hardcoded anymore.

--- DnDDMTools/Controllers/HomeController.cs
+++ DnDDMTools/Controllers/HomeController.cs
@@ -5,22 +5,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DnDDMTools.Data.Entities;
 
 namespace DnDDMTools.Controllers
 {
     public class HomeController : Controller
     {
+        private Entities db = new Entities();
         public ActionResult Index()
         {
-            SelectListItem[] Race;
-            SelectListItem Dwarf = new SelectListItem() { Text = "Dwarf", Value = "1" };
-            SelectListItem Elf = new SelectListItem() { Text = "Elf", Value = "2" };
-            SelectListItem Halfling = new SelectListItem() { Text = "Halfling", Value = "3" };
-            SelectListItem Human = new SelectListItem() { Text = "Human", Value = "4" };
-            SelectListItem Dragonborn = new SelectListItem() { Text = "Dragonborn", Value = "5" };
-
-            Race = new SelectListItem[5] { Dwarf, Elf, Halfling, Human, Dragonborn };
-            ViewBag.Race = Race;
+            var races = db.Races.Select(r=>r.Name);
+            ViewBag.Race = races;
 
             SelectListItem[] LevelArray = new SelectListItem[20];
             for (int i = 0; i <= 19; i++)
@@ -43,20 +38,10 @@ namespace DnDDMTools.Controllers
         [HttpPost]
         public JsonResult PopulateSubraces(int id)
         {
-            RaceModel[] subraces = GetSubRaces(id);
-            SelectListItem[] returnToView = null;
-
+            var subraces = db.SubRaces.Where(s=>s.RaceId == id).ToList();
             if (subraces != null)
             {
-                returnToView = new SelectListItem[subraces.Count()];
-                for (int i = 0; i < subraces.Count(); i++)
-                {
-                    returnToView[i] = new SelectListItem() { Text = subraces[i].Name, Value = (i).ToString() };
-                }
-            }
-            if (returnToView != null)
-            {
-                return Json(returnToView, JsonRequestBehavior.AllowGet);
+                return Json(subraces);
             }
             else
             {
@@ -67,55 +52,22 @@ namespace DnDDMTools.Controllers
         [HttpPost]
         public JsonResult SelectedSubrace(int mainId, int srId)
         {
-            RaceModel selectedSubrace = GetSelectedSubRace(mainId, srId);
+            SubRace selectedSubrace = GetSelectedSubRace(mainId, srId);
             return Json(selectedSubrace, JsonRequestBehavior.AllowGet);
         }
 
 
-        public RaceModel[] GetSubRaces(int id)
+        public IEnumerable<SubRace> GetSubRaces(int id)
         {
-            RaceModel[] subraces = null;
-            switch (id)
-            {
-                case 1:
-                    //user picked Dwarf
-                    RaceModel HillDwarf = new RaceModel(1,"Hill Dwarf", 0, 0, 2, 0, 1, 0, 4, 2, "25 feet or 5 5x5 squares", 12, 12);
-                    RaceModel MountainDwarf = new RaceModel(2,"Mountain Dwarf", 2, 0, 2, 0, 0, 0, 4, 2, "25 feet or 5 5x5 squares", 12, 12);
-                    subraces = new RaceModel[2] { HillDwarf, MountainDwarf };
-                    break;
-                case 2:
-                    //user picked elf
-                    RaceModel HighElf = new RaceModel(1,"High Elf", 0, 2, 0, 1, 0, 0, 6, 3, "30 feet or 6 5x5 squares", 12, 12);
-                    RaceModel WoodElf = new RaceModel(2,"Wood Elf", 0, 2, 0, 0, 1, 0, 6, 3, "35 feet or 7 5x5 squares", 12, 12);
-                    RaceModel Drow = new RaceModel(3,"Drow", 0, 2, 0, 0, 0, 1, 6, 3, "30 feet or 6 5x5 squares", 12, 12);
-                    subraces = new RaceModel[3] { HighElf, WoodElf, Drow };
-                    break;
-                case 3:
-                    //user picked halfling
-                    RaceModel Lightfoot = new RaceModel(1,"LightFoot", 0, 2, 0, 0, 0, 1, 3, 1, "25 feet or 5 5x5 squares", 6, 6);
-                    RaceModel Stout = new RaceModel(2,"Stout", 0, 2, 1, 0, 0, 0, 3,1, "25 feet or 5 5x5 squares", 6, 6);
-                    subraces = new RaceModel[2] { Lightfoot, Stout };
-                    break;
-                case 4:
-                    //Human :P boring right?
-                    RaceModel Human = new RaceModel(1,"Human", 1, 1, 1, 1, 1, 1, 6, 4, "30 feet or 6 5x5 squares", 6, 6);
-                    RaceModel HumanVARIANT = new RaceModel(2,"HumanVARIANT", 0, 0, 0, 0, 0, 0, 6, 4, "30 feet or 6 5x5 squares", 6, 6);
-                    //come back for the variant for noting that >user< picks his stat allocations as well as one feat and one skill
-                    subraces = new RaceModel[2] { Human, HumanVARIANT };
-                    break;
-                case 5:
-                    //Dragonborn 
-                    RaceModel DragonBornBasic = new RaceModel(1,"Dragonborn -no alts", 2, 0, 0, 0, 0, 1, 6, 3, "30 feet or 6 5x5 squares", 6, 12);
-                    subraces = new RaceModel[1] { DragonBornBasic };
-                    break;
-            }//end switch
+            var subraces = db.SubRaces.Where(t=>t.RaceId == id);
+            
             return subraces;
         }//end GetSubRace
 
-        public RaceModel GetSelectedSubRace(int mainId, int srId)
+        public SubRace GetSelectedSubRace(int mainId, int srId)
         {
-            RaceModel[] list = GetSubRaces(mainId);
-            return list[srId];
+            SubRace selected = db.SubRaces.Where(s=>s.RaceId == mainId && s.Id == srId).FirstOrDefault();
+            return selected;
         }
     }
 }
